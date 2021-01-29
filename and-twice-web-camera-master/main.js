06921db3701ace226997780c7a698fdd1e07a47a
Jung-Hwi-Hak <70425723+Jung-Hwi-Hak@users.noreply.github.com>
2021-01-29 10:31:48 +0900

    Update main.js

--- and-twice-web-camera-master/main.js
+++ and-twice-web-camera-master/main.js
@@ -1,8 +1,10 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+//const canvas = document.getElementById("canvas");
+//const ctx = canvas.getContext("2d");
 const camera = document.getElementById("camera");
 let mode = "user";
-let capturing = false;
+//let capturing = false;
+
+capture(mode);
 
 function capture(mode) {
     navigator.mediaDevices
@@ -17,13 +19,13 @@ function capture(mode) {
         });
 }
 
-function stopCamera() {
-    camera.srcObject && camera.srcObject.getTracks().forEach((t) => t.stop());
-}
-capture(mode);
+//function stopCamera() {
+//    camera.srcObject && camera.srcObject.getTracks().forEach((t) => t.stop());
+//}
+
 
 document.getElementById("switch").addEventListener("click", () => {
-    stopCamera();
+    //stopCamera();
     mode = `${mode === "user" ? "environment" : "user"}`;
     capture(mode);
 });
