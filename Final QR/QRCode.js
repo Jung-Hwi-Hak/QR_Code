97bd45f6df59b56b377467f79c6084bc70fd2864
Jung-Hwi-Hak <70425723+Jung-Hwi-Hak@users.noreply.github.com>
2021-01-31 19:45:55 +0900

    Add files via upload

---
+++ Final QR/QRCode.js
@@ -0,0 +1,36 @@
+var camera = document.getElementById('camera'); // video
+var change = document.getElementById('switch'); // Camera Change
+var change_count = 0;
+var msg = document.getElementById('msg2'); // QRCode Value;
+
+let scanner = new Instascan.Scanner({video: camera});
+scanner.addListener('scan', (content)=>{
+    $('#msg2').text(content);
+    console.log('nice');
+})
+//scan
+$('#switch').on('click', ()=>{
+    stopCamera();
+    change_count = '${change_count = 0 ? 1 : 0}';
+    console.log('asd');
+    test();
+})
+function stopCamera() { // mode를 변경하는 버튼을 누르면 정지 시키고
+    camera.srcObject && camera.srcObject.getTracks().forEach((t) => t.stop());
+}
+
+
+test();
+function test(){
+Instascan.Camera.getCameras().then(function(cameras){ //카메라 키기.
+if(cameras.length > 0){
+    scanner.start(cameras[change_count]);
+    console.log('aa');
+}else{
+    console.error('No cameras found');
+}
+}).catch(function(e){
+console.error(e);
+});
+}
+
