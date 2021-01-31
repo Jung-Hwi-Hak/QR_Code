3531560faa9ad1ebbb13369d7be98ba3d43dd6cd
Jung-Hwi-Hak <70425723+Jung-Hwi-Hak@users.noreply.github.com>
2021-01-31 17:36:51 +0900

    Update QRCode.js

--- QRCode/QRCode.js
+++ QRCode/QRCode.js
@@ -1,6 +1,14 @@
 const camera = document.getElementById("camera");
 let mode = "user";
 
+// let test = new Instascan.Camera.getCameras();
+let scanner = new Instascan.Scanner({video: document.getElementById("camera")}); //scan이 출력되는 구간 .
+
+scanner.addListener('scan',function(content){ // 스캔을 통한 값 content를 #msg2에 입력.,
+$('#msg2').text(content);
+console.log(content);
+});
+
 function capture(mode) {
     navigator.mediaDevices
         .getUserMedia({
@@ -11,11 +19,13 @@ function capture(mode) {
         })
         .then((stream) => {
             camera.srcObject = stream;
+            console.log('start');
         });
 }
 
 function stopCamera() {
     camera.srcObject && camera.srcObject.getTracks().forEach((t) => t.stop());
+    console.log('change_mode');
 }
 
 capture(mode);
@@ -29,17 +39,12 @@ document.getElementById("switch").addEventListener("click", () => { //카메라 
 
 
 //scan
-let scanner = new Instascan.Scanner({video: document.getElementById('camera')}); //scan이 출력되는 구간 .
-scanner.addListener('scan',function(content){ // 스캔을 통한 값 content를 #msg2에 입력.,
-$('#msg2').text(content);
+Instascan.Camera.getCameras().then(function(camera){ //카메라 키기.
+if(camera.length > 0){
+    scanner.start(camera[0]);
+}else{
+    console.error('No cameras found');
+}
+}).catch(function(e){
+console.error(e);
 });
-
-// Instascan.Camera.getCameras().then(function(cameras){ //카메라 키기.
-// if(cameras.length > 0){
-//     scanner.start(cameras[0]);
-// }else{
-//     console.error('No cameras found');
-// }
-// }).catch(function(e){
-// console.error(e);
-// });
