c5e1a600b4b7dfab79ba64bedebf62c21234ff62
Jung-Hwi-Hak <70425723+Jung-Hwi-Hak@users.noreply.github.com>
2021-02-02 10:23:22 +0900

    Update QRCode.js

--- Final QR/QRCode.js
+++ Final QR/QRCode.js
@@ -6,7 +6,7 @@ var msg = document.getElementById('msg2'); // QRCode Value;
 let scanner = new Instascan.Scanner({video: camera});
 scanner.addListener('scan', (content)=>{
     $('#msg2').text(content);
-    console.log('nice');
+    
 })
 //scan
 
@@ -17,24 +17,20 @@ $('#switch').on('click', ()=>{
     }else{
         change_count = 0;
     }
-    // `${mode === "user" ? "environment" : "user"}`;
-    console.log(change_count);
-    alert('hiih');
     test();
 })
 function stopCamera() { // mode를 변경하는 버튼을 누르면 정지 시키고
     camera.srcObject && camera.srcObject.getTracks().forEach((t) => t.stop());
 }
 
-
-alert('change');
+alert('change???');
 test();
 
 function test(){
 Instascan.Camera.getCameras().then(function(cameras){ //카메라 키기.
 if(cameras.length > 0){
     scanner.start(cameras[change_count]);
-    console.log('aa');
+ 
 }else{
     console.error('No cameras found');
 }
