b46b5be4d21deeee91cef45c435385c713ca32a0
Jung-Hwi-Hak <70425723+Jung-Hwi-Hak@users.noreply.github.com>
2021-02-02 10:40:29 +0900

    Update QRCode.js

--- Final QR/QRCode.js
+++ Final QR/QRCode.js
@@ -1,36 +1,39 @@
 var camera = document.getElementById('camera'); // video
 var change = document.getElementById('switch'); // Camera Change
-var change_count = 2;
-var msg = document.getElementById('msg2'); // QRCode Value;
+var change_camera = 2;
+var msg = document.getElementById('msg'); // QRCode Value;
+var check_facing = document.getElementById('check_facing');
 
 let scanner = new Instascan.Scanner({video: camera});
 scanner.addListener('scan', (content)=>{
-    $('#msg2').text(content);
-    
+    $('#msg').text(content);
 })
 //scan
 
+check_facing.innerText = "후면";
 $('#switch').on('click', ()=>{
     stopCamera();
     if(change_count != 2){
         change_count = 2;
+        check_facing.innerText = "후면";
     }else{
+        check_facing.innerText = "전면";
         change_count = 0;
+
     }
+    console.log(change_camera);
     test();
 })
 function stopCamera() { // mode를 변경하는 버튼을 누르면 정지 시키고
     camera.srcObject && camera.srcObject.getTracks().forEach((t) => t.stop());
 }
 
-alert('change???');
-test();
 
+test();
 function test(){
 Instascan.Camera.getCameras().then(function(cameras){ //카메라 키기.
 if(cameras.length > 0){
-    scanner.start(cameras[change_count]);
- 
+    scanner.start(cameras[change_camera]);
 }else{
     console.error('No cameras found');
 }
